dab1f90f2ccff23cc5a6d143592ac989651727ab
CoderChicken1234 <88574701+CoderChicken1234@users.noreply.github.com>
2021-08-17 21:22:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var Spritey;
+
+function preload (){
+}
+
+  function setup() {
+    createCanvas(400,200);
+    
+
+  }
+
+  function draw() 
+  {
+    Spritey = createSprite(200,200,20,50);
+    background("violet")
+
+
+  }
+
+
+
+
